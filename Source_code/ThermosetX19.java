9a1307e98b397c42bded9c78f5541d9ceca103d2
hieuvu99 <64103982+hieuvu99@users.noreply.github.com>
2021-02-16 14:25:54 -0500

    Add files via upload

---
+++ Source_code/ThermosetX19.java
@@ -0,0 +1,78 @@
+package itec3030.assignments.a1.sensors.thermoset.ThermoSetX19;
+
+
+import itec3030.assignments.a1.sensors.thermoset.ThermoSetX19FrontEnd.FrontEnd;
+import itec3030.smarthome.standards.*;
+public class ThermosetX19 implements Thermostat{ 
+	private ControllerInterface controllerInterface;
+	private String ID;
+	private int temperature;
+	private boolean enable;
+	private FrontEnd UI;
+	
+	public ThermosetX19() {
+		// Create new ThermoSetX19 device
+		UI = new FrontEnd(this);
+		UI.pack();
+		UI.setVisible(true);
+		
+	}
+
+	@Override
+	public void disable() {
+		// TODO disable the machine
+		this.enable = false;
+	}
+
+	@Override
+	public void enable() {
+		// TODO enable the machine
+		this.enable = true;
+	}
+
+	@Override
+	public boolean enabled() {
+		// TODO check the status of the devide whether it enable or disable
+		return enable;
+	}
+
+	@Override
+	public ControllerInterface getController() {
+		// TODO return the controller interface of the device
+		return this.controllerInterface;
+	}
+
+	@Override
+	public String getID() {
+		// TODO return ID of the device
+		return this.ID;
+	}
+
+	@Override
+	public void setController(ControllerInterface controllerInterface) {
+		// TODO set the controller interface for the device
+		this.controllerInterface = controllerInterface;
+	}
+
+	@Override
+	public void setID(String id) {
+		// TODO set device ID
+		this.ID = id;
+		
+		
+	}
+
+	@Override
+	public int getReading() {
+		// TODO return current reading temperature from the device
+		return temperature;
+	}
+
+	@Override
+	public void newTemperature(int temperature) {
+		// TODO set a new temperature for the device
+		this.temperature = temperature;
+	}
+	
+
+}
